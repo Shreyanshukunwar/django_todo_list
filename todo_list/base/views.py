b69a87fc7c318c60bdb53e507573bb87f64c87f6
Shreyanshukunwar <shreyanshukunwar@gmail.com>
2021-05-18 15:29:39 +0545

    added CBVs for CRUD operations

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy #redirects user to certain part of page
 
 from .models import *
 
@@ -9,3 +13,24 @@ from .models import *
 
 class TaskList(ListView):
 	model = Task
+	context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+	model = Task
+	context_object_name = 'task'
+	template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+	model = Task
+	fields = '__all__'
+	success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+	model = Task
+	fields = '__all__'
+	success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+	model = Task
+	context_object_name = 'task'
+	success_url = reverse_lazy('tasks')
